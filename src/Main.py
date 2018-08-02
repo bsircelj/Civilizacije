0d015aa94544b08c81f4786a8b37b1d9715ee74d
Beno Šircelj <benosircelj@gmail.com>
2018-08-02 12:29:39 +0200

    Smothed graph properly.

--- src/Main.py
+++ src/Main.py
@@ -8,23 +8,41 @@ from lognormal import lognormal
 from sampleMultiple import sampleMultiple
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.signal as sig
+import scipy.ndimage.filters as fl
 
 size = 1000;
 
 Rstar = loguniform(1,100,size)
 Fplanets = loguniform(0.1,1,size)
 Nhabitable = loguniform(0.1,1,size)
-Flife = lognormal(0.000000000000000000001,1,size)
+Flife = lognormal(10**(-200),1,size)
+#Flife = loguniform(1,1000,size)
 Fintelligence = loguniform(0.001,1,size)
 Fcivilization = loguniform(0.01,1,size)
 Length = loguniform(100,10000000000,size)
 
-(xaxis, final) = sampleMultiple([Rstar,Fplanets,Nhabitable,Flife,Fintelligence,Fcivilization,Length],size,1000)
+#(xaxis, final) = sampleMultiple([Rstar,Fplanets,Nhabitable,Flife,Fintelligence,Fcivilization,Length],size,1000)
+#(xaxis, final) = sampleMultiple([Rstar,Fplanets,Nhabitable,Fintelligence,Fcivilization,Length],size,1000)
 
-plt.plot(xaxis,sig.savgol_filter(final,51,10))
-##plt.plot(xaxis,final)
+
+tog = [Rstar,Fplanets,Nhabitable,Flife,Fintelligence,Fcivilization,Length]
+(xaxis, final) = sampleMultiple(tog[0:4],size,1000)
+
+
+final = fl.gaussian_filter(final,5)
+mean = 0
+sum = 0
+for p in range(1,len(final)):
+    print(xaxis[p]," - ",final[p])
+    mean += p*final[p]
+    sum += final[p]
+
+
+print("\nMean: ",xaxis[int(mean/sum)])
+plt.plot(xaxis,final)
 plt.xscale("log")
 plt.show()
+
+
 
 
--- src/loguniform.py
+++ src/loguniform.py
@@ -7,7 +7,8 @@ def loguniform(low=0.01, high=1, size=10, base=np.exp(1)):
     pdf = 1/(pdf * (np.log(high)- np.log(low)))
     return  (low,high,pdf/sum(pdf))
 
-#f = loguniform(0.001,1,1000000)[0:2]
+#f = loguniform(1,100,1000)[2]
+#plt.xscale("log")
 #plt.plot(f)
 #plt.show()
 
