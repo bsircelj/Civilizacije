6101580d88efd55749574625c02553ca90aa0e3c
JurijNastran <jurij.nastran@gmail.com>
2018-08-08 15:09:25 +0200

    Improved lifeDist and mpLogNormal

--- src/lifeDist.py
+++ src/lifeDist.py
@@ -4,17 +4,19 @@ Created on 7 Aug 2018
 @author: benos
 '''
 from mpLogNormal import mpLogNormal
-from sampleMultiple import sampleMultiple 
-from sampleMultiple import sample
+from mpLogUniform import mpLogUniform
+from mpSampleMultiple import mpSampleMultiple 
+from mpSampleMultiple import sample
 import numpy as np
 import matplotlib.pyplot as plt
+from mpmath import *
 
 
 def lifeDist(times):
     size = 1000
-    lamb = mpLogNormal(10 ** (-100), 10 ** (100), size, 1, 10 ** 9.99)
-    V = mpLogNormal(10 ** (-30), 10 ** (20), size)
-    t = mpLogNormal(10 ** (7), 10 ** (10), size)
+    lamb = mpLogNormal(10 ** (-10), 10 ** (10), size, 1, 10 ** 50)
+    V = mpLogUniform(10 ** (-35), 10 ** (15), size)
+    t = mpLogUniform(10 ** (14), 10 ** (17), size)
     dist = [0] * times
     val = 1
     izpis = 0
@@ -26,19 +28,21 @@ def lifeDist(times):
         val *= sample(lamb)
         val *= sample(V)
         val *= sample(t)        
-        val = 1 - np.exp(-(val))
+        val = 1 - mp.exp(-(val))
                 
         dist[int((times - 1) * val)] += 1    
     return dist
 
+size = 1000
+y = lifeDist(size)
 
-#size = 1000
-#y = lifeDist(size)
+x = mp.linspace(0, 1, size)
 
-#x = np.linspace(0, 1, size)
+plt.plot(x, y)
+plt.xscale("log")
+plt.xlim(10**(-3) , 1)
+plt.ylim(0,0000.1)
+plt.show()
 
-#plt.plot(x, y)
-# plt.xscale("log")
-#plt.show()
-    
+  
     
