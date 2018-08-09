d04e97f78b424746b399ba116e0b0d3d85345b37
JurijNastran <jurij.nastran@gmail.com>
2018-08-09 14:15:28 +0200

    Short fix of the previous commit.

---
+++ src/mpLogNormal.py
@@ -0,0 +1,66 @@
+'''
+Created on 8 Aug 2018
+
+@author: benos
+'''
+import numpy as np
+from math import *
+#from scipy import mpmath
+from mpmath import *
+import matplotlib.pyplot as plt
+mp.dps = 5
+
+def mpLogNormal(low=0.000000000000000000001, high=1, size=1000, median=1.0, sigma=mpmathify(10**50) ):
+    low = mpmathify(10**(-188))
+    high = mpf('1.0')
+    mu = mp.log(median, e)
+    mu = mpf('1')
+    
+    #import matplotlib.pyplot as plt
+
+    '''
+    lahko to uporabis
+    newList = [x / myInt for x in myList]
+    
+    
+    
+    pdf = (mp.exp(-(mp.log(x) - mu) ** 2 / (2 * sigma ** 2))
+           / (x * sigma * mp.sqrt(2 * mp.pi)))
+    
+
+    return (min, max, pdf)
+    '''
+    pdf = mp.linspace(low, high, size)
+    sum = mpf('0')
+    for i in range(0,len(pdf)):
+        #pdf[i] = mpf('1.0')/(pdf[i] * (mp.log(high)- mp.log(low)))
+        pdf[i] = (mp.exp(-(mp.log(pdf[i]) - mu) ** 2 / (2 * sigma * sigma )) / (pdf[i] * sigma * mp.sqrt(2 * mp.pi)))
+        sum += pdf[i]
+    return  (low,high,[x/sum for x in pdf])
+'''
+size = 1000
+x = mp.linspace(mpf('10')**(-188) , mpf('1'), size)
+lamb = mpLogNormal( mpf('10')**(-188) , mpf('1'), size, mpf('1'), mpf('10')**50 )
+print(x)
+print(lamb)
+low = mpf('10') ** (-188)
+print(low)
+
+'''
+
+size = 1000
+low = mpmathify(mpf('10')**(-188))
+high = mpf('1.0')
+x = mp.linspace(log(low) , log(high), size)
+x = [ exp(kos) for kos in x]
+lamb = mpLogNormal( mpf('10')**(-188) , mpf('1'), size, mpf('1'), mpmathify(10**50) )[2]
+print(x)
+print(lamb)
+plt.plot(x, lamb)
+plt.xscale('log')
+plt.xlim(10**(-188),1)
+plt.show()
+#plot(lamb, xlim=[low, high])
+
+
+
