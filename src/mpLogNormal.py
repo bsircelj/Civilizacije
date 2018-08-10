bffba662daf76512b12a0b6e61488abe1754df13
JurijNastran <jurij.nastran@gmail.com>
2018-08-10 14:52:28 +0200

    Completed the mpLogNormal method.

--- src/mpLogNormal.py
+++ src/mpLogNormal.py
@@ -5,63 +5,36 @@ Created on 8 Aug 2018
 '''
 import numpy as np
 from math import *
-#from scipy import mpmath
 from mpmath import *
 import matplotlib.pyplot as plt
-mp.dps = 5
+mp.dps = 200
 from mpLogspace import mpLogspace
 
-def mpLogNormal(low=0.000000000000000000001, high=1, size=1000, median=1.0, sigma=mpmathify(10**50) ):
-    low = mpmathify(10**(-188))
-    high = mpf('1.0')
-    mu = mp.log(median, e)
-    mu = mpf('1')
-    
-    #import matplotlib.pyplot as plt
+def mpLogNormal(low=mpmathify(10**(-188)), high=mpmathify(10**12), size=100000, median=mpmathify(1.0), sigma=mpmathify(10**50) ):
 
-    '''
-    lahko to uporabis
-    newList = [x / myInt for x in myList]
-    
-    
-    
-    pdf = (mp.exp(-(mp.log(x) - mu) ** 2 / (2 * sigma ** 2))
-           / (x * sigma * mp.sqrt(2 * mp.pi)))
-    
+    mu, sigma = mpf('0'), mpmathify(10**50) #mean and standard deviation
+    x = np.logspace( -188, 12, size)
 
-    return (min, max, pdf)
-    '''
-    pdf = mpLogspace(low, high, size)
-    #summ = mpf('0')
+    pdf = mp.linspace(1, size, size)
+    sum = mpf('0')
     for i in range(0,len(pdf)):
-        #pdf[i] = mpf('1.0')/(pdf[i] * (mp.log(high)- mp.log(low)))
-        pdf[i] = (mp.exp(-(mp.log(pdf[i]) - mu) ** 2 / (2 * sigma * sigma )) / (pdf[i] * sigma * mp.sqrt(2 * mp.pi)))
-        #summ = mp.fadd(summ,pdf[i])
-    return  (low,high,[x/mp.fsum(pdf) for x in pdf])
-'''
-size = 1000
-x = mp.linspace(mpf('10')**(-188) , mpf('1'), size)
-lamb = mpLogNormal( mpf('10')**(-188) , mpf('1'), size, mpf('1'), mpf('10')**50 )
-print(x)
-print(lamb)
-low = mpf('10') ** (-188)
-print(low)
+        pdf[i] = (mp.exp(-(mp.log(x[i]) - mu) ** 2 / (2 * sigma * sigma )) / ( x[i] * sigma * mp.sqrt(2 * mp.pi)))
+        sum += pdf[i]
+
+    for i in range(0,len(pdf)):
+        pdf[i] = pdf[i] / sum
+
+
+    return (low, high, pdf)
+    
+    
 
 '''
-'''
-size = 10
-low = mpmathify(mpf('10')**(-188))
-high = mpf('1.0')
-x = mp.linspace(mp.log(low) , mp.log(high), size)
-x = [ mp.exp(kos) for kos in x]
-lamb = mpLogNormal( mp.log(mpf('10')**(-188)) , mp.log(mpf('1')), size, mpf('1'), mpmathify(10**50) )[2]
-print(x)
-print(lamb)
-plt.plot(x, lamb)
+x = np.logspace(-188, 12, 100000)
+pdf=mpLogNormal()[2]
+
+plt.plot(x, pdf, linewidth=2, color='r')
 plt.xscale('log')
-plt.xlim(10**(-188),1)
 plt.show()
-#plot(lamb, xlim=[low, high])
-
 '''
 
