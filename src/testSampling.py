19f03d959a013f6decaa328d2b9839d51f2ad028
Beno Šircelj <benosircelj@gmail.com>
2018-08-14 10:18:30 +0200

    Added tests for sampling.

--- src/testSampling.py
+++ src/testSampling.py
@@ -10,17 +10,36 @@ import scipy.ndimage.filters as fl
 import matplotlib.pyplot as plt
 from createGraph import createGraph
 from mpLogUniform import mpLogUniform
+from mpSampleMultiple import *
 from mpLogspace import mpLogspace
 
-dist = mpLogUniform(0.1,1,1000)
+size = 1000
 
-sampledDist = ...
+dist = mpLogUniform(0.1,1,size)
+x = mpLogspace(dist[0],dist[1],size)
+#plt.subplot(2,1,1)
+plt.plot(x,dist[2])
 
 
-x = mpLogspace(dist[0],dist[1])
-plt.subplot()
+times = 1000
 
-plt.subplot(2,1,2)
-plt.plot(xS,yS)
+
+yS = [0]*size
+for i in range(0,times):
+    val = sampleByBisection(StandardiseDistribution(dist))
+    #val = sample(dist)
+    
+    for p in range(0,size):
+        if x[p]<val:
+            yS[p]+=1
+            
+yS = [x/sum(yS) for x in yS]
+    
+
+
+#plt.subplot(2,1,2)
+plt.plot(x,yS)
+
+plt.show()
 
 
