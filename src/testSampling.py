7dbb057de5ddcbb9b8ebebfd998b0ccbebef10cc
Beno Šircelj <benosircelj@gmail.com>
2018-08-14 09:54:22 +0200

    Removed csv from gitignore.

--- src/testSampling.py
+++ src/testSampling.py
@@ -10,8 +10,17 @@ import scipy.ndimage.filters as fl
 import matplotlib.pyplot as plt
 from createGraph import createGraph
 from mpLogUniform import mpLogUniform
+from mpLogspace import mpLogspace
 
 dist = mpLogUniform(0.1,1,1000)
 
 sampledDist = ...
+
+
+x = mpLogspace(dist[0],dist[1])
+plt.subplot()
+
+plt.subplot(2,1,2)
+plt.plot(xS,yS)
+
 
