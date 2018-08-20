4c0d7f864263f8b81432c01d76c29ff8238fab93
JurijNastran <jurij.nastran@gmail.com>
2018-08-20 12:18:02 +0200

    Modified ToyModel.

--- src/ToyModel.py
+++ src/ToyModel.py
@@ -2,17 +2,31 @@ import numpy as np
 import mpmath as mp
 import random
 from mpmath import mpmathify
+import matplotlib.pyplot as plt
+mp.dps=8
+size = 100000
+one = mpmathify(1.0)
+hundretBilion = 100000000000
 
 def getArrayOfProducts(size):
     arrayOfParameters =[]
-    for j in range(0, size ):
+    stevecManjsihOd1 = 0
     
+    for j in range(0, size ):
+        
         parameters = mpmathify(1.0)
         for i in range(0,9):
             r = random.uniform(0, 0.2)
             parameters *= r
-        parameters = parameters * 100000000
+            
+        parameters = parameters * ( 100000000000 )  
+        #if parameters<1:
+        #    stevecManjsihOd1+=1
+        stevecManjsihOd1+=(parameters<0.7)
+        alonePossibility = stevecManjsihOd1/size
         arrayOfParameters.append( parameters )
-    return arrayOfParameters
+    return (arrayOfParameters, alonePossibility )
 
-print(getArrayOfProducts(10000))
+alonePossibility = getArrayOfProducts(size)[1]
+print(alonePossibility)
+#print(arrayOfProducts)
