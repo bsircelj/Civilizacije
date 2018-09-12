461de30638a42ea130ef64444c68726912794b38
Beno Šircelj <benosircelj@gmail.com>
2018-09-12 12:20:12 +0200

    Exponts with Laplace cutoff.

--- src/ExponentsAlternative.py
+++ src/ExponentsAlternative.py
@@ -1,46 +1,56 @@
-from mpSampleMultiple import mpSampleMultiple, mpSampleMultipleTime, sampleL
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.ndimage.filters as fl
-from lifeDist import lifeDist, lifeDist2
-from mpmath import mpmathify
 import mpmath as mp
-import time
-from meanMedian import meanMedian
-from IO import save, readFile
 import random
-from ToyModel import getCDFNIC, getMaxPDF, normalizePDF
 import math
-import pylab as p
-from mpLogspace import mpLogspace
-mp.dps=230
+from lifeDist import lifeDist, lifeDist2
+
+mp.dps = 230
 
 
 def getNEksponentSample():
-    RStarSample = random.uniform( 0 , 2 )
-    fPlanets = random.uniform( -1 , 0 )
-    nEnvironment = random.uniform( -1 , 0 )
-    fInteligence = random.uniform( -3 , 0 )
-    fCivilization = random.uniform( -2 , 0 )
-    L = random.uniform( 2 , 10 )
+    RStarSample = random.uniform(0 , 2)
+    fPlanets = random.uniform(-1 , 0)
+    nEnvironment = random.uniform(-1 , 0)
+    fInteligence = random.uniform(-3 , 0)
+    fCivilization = random.uniform(-2 , 0)
+    L = random.uniform(2 , 10)
     
-    fLife = lifeDist(vMin=0,vMax=15,tMin=14,tMax=17,mean=0, sigma=200)
-    fLifeEks = float( mp.log(fLife, 10) )
+    fLife = lifeDist(vMin=0, vMax=15, tMin=14, tMax=17, mean=0, sigma=200)
+    fLifeEks = float(mp.log(fLife, 10))
+    
+    # modification
+    skip = 0
+    mini = -RStarSample - L
+    for it in (fPlanets, nEnvironment, fInteligence, fCivilization, fLifeEks):
+        if it < mini:
+            skip = 1
+            break
+    # modification    
+    
     resitev = RStarSample + fPlanets + nEnvironment + fLifeEks + fInteligence + fCivilization + L
-    return resitev
+    return (skip, resitev)
 
-def getDistributionOfEks( size = 1000, pdfSize = 2151, low = -15, high = 15):
-    xOs = np.linspace(low, high, pdfSize )
-    pdf= [0] * pdfSize
+
+def getDistributionOfEks(size=1000, pdfSize=2151, low=-15, high=15):
+    xOs = np.linspace(low, high, pdfSize)
+    pdf = [0] * pdfSize
     zmnozek = ((pdfSize - 1) / (high - low))  # =1000
     pristevek = low * zmnozek  # =5000
+    izpis = 0
     for i in range(0, size):
-        parameters = getNEksponentSample()
+        (skip, parameters) = getNEksponentSample()
         
+        if i % (size / 10) == 0:
+            print(izpis, "%")
+            izpis += 10
+        
+        if skip:
+            continue
+                
         if (math.isinf(parameters)):
             pdf[0] += 1
             continue
-        zaokrozenIndeks = round(parameters * zmnozek - pristevek )
+        zaokrozenIndeks = round(parameters * zmnozek - pristevek)
         if (zaokrozenIndeks < 0):
             pdf[ 0 ] += 1
             continue
@@ -50,40 +60,8 @@ def getDistributionOfEks( size = 1000, pdfSize = 2151, low = -15, high = 15):
         if (math.isinf(zaokrozenIndeks)):
             pdf[0] += 1
             continue
-        indeksPDF = int( zaokrozenIndeks )
+        indeksPDF = int(zaokrozenIndeks)
         pdf[ indeksPDF ] += 1
     
     return xOs, pdf
-
-#dejanski program:
-start = -40
-stop = 15
-pdfSize = 2151
-size = 10000
-
-xOs, pdf = getDistributionOfEks(size, pdfSize, low = start, high = stop)
-cdf = getCDFNIC(pdf)
-pdf[0] = pdf[1]
-pdf=normalizePDF(pdf)
-pdf = fl.gaussian_filter( pdf , 1)
-#cdf = getCDFNIC(pdf)
-
-
-l = mp.linspace( start, stop, pdfSize )
-xOs = [mp.power(10, x) for x in l]
-
-save(xOs, pdf , "pdf-sigma 200")
-save(xOs, cdf , "cdf-sigma 200")
-
-plt.xscale('log')
-plt.plot(xOs, pdf, 'blue', label = 'pdf' )
-plt.plot(xOs, cdf, 'red', label = 'cdf' )
-p.fill(xOs, pdf, facecolor='blue', alpha=0.5)
-plt.xlim(10**start , 10**stop)
-plt.show()
-
-
-
-
-
 
---
+++ src/ExponentsMain.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import scipy.ndimage.filters as fl
+from IO import save
+from ToyModel import getCDFNIC, normalizePDF
+import mpmath as mp
+import pylab as p
+from ExponentsAlternative import getDistributionOfEks
+#dejanski program:
+start = -40
+stop = 15
+pdfSize = 2151
+size = 100000
+
+xOs, pdf = getDistributionOfEks(size, pdfSize, low = start, high = stop)
+cdf = getCDFNIC(pdf)
+pdf[0] = pdf[1]
+pdf=normalizePDF(pdf)
+l = mp.linspace( start, stop, pdfSize )
+xOs = [mp.power(10, x) for x in l]
+
+save(xOs, pdf , "pdf skipped 200")
+save(xOs, cdf , "cdf skipped 200")
+
+
+pdf = fl.gaussian_filter( pdf , 20)
+#cdf = getCDFNIC(pdf)
+
+
+plt.xscale('log')
+plt.plot(xOs, pdf, 'blue', label = 'pdf' )
+plt.plot(xOs, cdf, 'red', label = 'cdf' )
+p.fill(xOs, pdf, facecolor='blue', alpha=0.5)
+plt.xlim(10**start , 10**stop)
+plt.show()
+
